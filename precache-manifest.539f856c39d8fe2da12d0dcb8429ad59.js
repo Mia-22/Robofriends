e8eba961cd1c5a5050f4a06622b09de6c2b81223
Мирјана МР. Ристова <mirjana@codex.mk>
2019-11-26 11:30:20 +0100

    Updates

---
+++ precache-manifest.539f856c39d8fe2da12d0dcb8429ad59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57bbd37f0c3768fd8de5a767aa1c49c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5efa7c78c8a7824bae90",
+    "url": "/robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/robofriends/static/js/2.36d0c395.chunk.js"
+  },
+  {
+    "revision": "5efa7c78c8a7824bae90",
+    "url": "/robofriends/static/js/main.5986c719.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
